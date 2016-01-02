43d4e668e9e4c091b9db8f507bb6613e6f129e91
Farbod Farshidian <farbod.farshidian@gamil.com>
2016-01-02 19:04:14 +0100

    update

--- src/test/IntegrationTest.cpp
+++ src/test/IntegrationTest.cpp
@@ -6,6 +6,11 @@
  */
 
 #include <memory>
+#include <fstream>
+
+#include <cereal/archives/xml.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/Eigen.hpp>
 
 #include "integration/Integrator.h"
 #include "dynamics/ControlledSystemBase.h"
@@ -53,14 +58,32 @@ int main (int argc, char* argv[])
 
 	std::vector<double> timeTrajectory;
 	std::vector<Eigen::Matrix<double,2,1>, Eigen::aligned_allocator<Eigen::Matrix<double,2,1>> > stateTrajectory;
+	std::vector<Eigen::Matrix<double,1,1>, Eigen::aligned_allocator<Eigen::Matrix<double,1,1>> > timeEigenTrajectory;
 
 	Eigen::Matrix<double,2,1> x0;
 	x0.setZero();
 
 	bool flag = ode45.integrate(x0, 0.0, 10.0, stateTrajectory, timeTrajectory);
 
+	timeEigenTrajectory.resize(timeTrajectory.size());
 	for (size_t i=0; i<timeTrajectory.size(); i++) {
 		std::cout << "At time " <<  timeTrajectory[i] << "\t state is: " << stateTrajectory[i].transpose() << std::endl;
+		timeEigenTrajectory[i](0) = timeTrajectory[i];
+	}
+
+	std::string resultDir = "/home/farbod/Programs/ct_ws/src/c_ocs2/cereal/test/integration_test";
+	std::string secondOrderStateFile = resultDir + "/secondOrderState.xml";
+	std::string secondOrderTimeFile = resultDir + "/secondOrderTime.xml";
+
+
+	{ // we need these brackets to make sure the archive goes out of scope and flushes
+		std::ofstream xmlSecondOrderState(secondOrderStateFile);
+		cereal::XMLOutputArchive archive_secondOrderState_xml(xmlSecondOrderState);
+		archive_secondOrderState_xml(CEREAL_NVP(stateTrajectory));
+
+		std::ofstream xmlSecondOrderTime(secondOrderTimeFile);
+		cereal::XMLOutputArchive archive_secondOrderTime_xml(xmlSecondOrderTime);
+		archive_secondOrderTime_xml(CEREAL_NVP(timeEigenTrajectory));
 	}
 
 }

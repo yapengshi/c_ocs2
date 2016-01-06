ac228fc8fe8e51ae264be4e246c103fdc742251a
Farbod Farshidian <farbod.farshidian@gamil.com>
2016-01-06 14:48:52 +0100

    update

---
+++ include/test/EXP2.h
@@ -0,0 +1,139 @@
+/*
+ * EXP2.h
+ *
+ *  Created on: Jan 6, 2016
+ *      Author: farbod
+ */
+
+#ifndef EXP2_H_
+#define EXP2_H_
+
+#include "GSLQ/GLQP.h"
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_Sys1 : public ControlledSystemBase<2,1>
+{
+public:
+	EXP2_Sys1() {}
+	~EXP2_Sys1() {}
+
+	void computeDerivative( const double& t, const Eigen::Vector2d& x, const Eigen::Matrix<double,1,1>& u, Eigen::Vector2d& dxdt)  {
+		Eigen::Matrix2d A;
+		A << 0.6, 1.2, -0.8, 3.4;
+		Eigen::Vector2d B;
+		B << 1, 1;
+
+		dxdt = A*x + B*u;
+	}
+
+	std::shared_ptr<ControlledSystemBase<2, 1> > clone() const { return std::make_shared<EXP2_Sys1>(*this); }
+};
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_Sys2 : public ControlledSystemBase<2,1>
+{
+public:
+	EXP2_Sys2() {}
+	~EXP2_Sys2() {}
+
+	void computeDerivative( const double& t, const Eigen::Vector2d& x, const Eigen::Matrix<double,1,1>& u, Eigen::Vector2d& dxdt)  {
+		Eigen::Matrix2d A;
+		A << 4, 3, -1, 0;
+		Eigen::Vector2d B;
+		B << 2, -1;
+
+		dxdt = A*x + B*u;
+	}
+
+	std::shared_ptr<ControlledSystemBase<2, 1> > clone() const { return std::make_shared<EXP2_Sys2>(*this); }
+};
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_SysDerivative1 : public DerivativesBase<2,1>
+{
+public:
+	EXP2_SysDerivative1() {};
+	~EXP2_SysDerivative1() {};
+
+	void getDerivativeState(state_matrix_t& A)  { A << 0.6, 1.2, -0.8, 3.4; }
+	void getDerivativesControl(control_gain_matrix_t& B) { B << 1, 1; }
+
+	std::shared_ptr<DerivativesBase<2,1> > clone() const { return std::make_shared<EXP2_SysDerivative1>(*this); }
+};
+
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_SysDerivative2 : public DerivativesBase<2,1>
+{
+public:
+	EXP2_SysDerivative2() {};
+	~EXP2_SysDerivative2() {};
+
+	void getDerivativeState(state_matrix_t& A)  { A << 4, 3, -1, 0; }
+	void getDerivativesControl(control_gain_matrix_t& B) { B << 2, -1; }
+
+	std::shared_ptr<DerivativesBase<2,1> > clone() const { return std::make_shared<EXP2_SysDerivative2>(*this); }
+};
+
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_CostFunction1 : public CostFunctionBase<2,1>
+{
+public:
+	EXP2_CostFunction1() {};
+	~EXP2_CostFunction1() {};
+
+	void evaluate(scalar_t& L) { L = 0.5*(x_(2)-2.0)*(x_(2)-2.0) + 0.5*u_(1)*u_(1); }
+
+	void stateDerivative(state_vector_t& dLdx) { dLdx << 0.0, (x_(2)-2.0); }
+	void stateSecondDerivative(state_matrix_t& dLdxx)  { dLdxx << 0.0, 0.0, 0.0, 1.0; }
+	void controlDerivative(control_vector_t& dLdu)  { dLdu << u_; }
+	void controlSecondDerivative(control_matrix_t& dLduu)  { dLduu << 1.0; }
+
+	void stateControlDerivative(control_feedback_t& dLdxu) { dLdxu.setZero(); }
+
+	void terminalCost(scalar_t& Phi) { Phi = 0; }
+	void terminalCostStateDerivative(state_vector_t& dPhidx)  { dPhidx.setZero(); }
+	void terminalCostStateSecondDerivative(state_matrix_t& dPhidxx)  { dPhidxx.setZero(); }
+
+	std::shared_ptr<CostFunctionBase<2,1> > clone() const { return std::make_shared<EXP2_CostFunction1>(*this); };
+};
+
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+class EXP2_CostFunction2 : public CostFunctionBase<2,1>
+{
+public:
+	EXP2_CostFunction2() {};
+	~EXP2_CostFunction2() {};
+
+	void evaluate(scalar_t& L) { L = 0.5*(x_(2)-2.0)*(x_(2)-2.0) + 0.5*u_(1)*u_(1); }
+
+	void stateDerivative(state_vector_t& dLdx) { dLdx << 0.0, (x_(2)-2.0); }
+	void stateSecondDerivative(state_matrix_t& dLdxx)  { dLdxx << 0.0, 0.0, 0.0, 1.0; }
+	void controlDerivative(control_vector_t& dLdu)  { dLdu << u_; }
+	void controlSecondDerivative(control_matrix_t& dLduu)  { dLduu << 1.0; }
+
+	void stateControlDerivative(control_feedback_t& dLdxu) { dLdxu.setZero(); }
+
+	void terminalCost(scalar_t& Phi) { Phi = 0.5*(x_(1)-4.0)*(x_(1)-4.0) + 0.5*(x_(2)-2.0)*(x_(2)-2.0); }
+	void terminalCostStateDerivative(state_vector_t& dPhidx)  { dPhidx << (x_(1)-4.0), (x_(2)-2.0); }
+	void terminalCostStateSecondDerivative(state_matrix_t& dPhidxx)  { dPhidxx.setIdentity(); }
+
+	std::shared_ptr<CostFunctionBase<2,1> > clone() const { return std::make_shared<EXP2_CostFunction2>(*this); };
+
+};
+
+#endif /* EXP2_H_ */
---
+++ src/test/EXP2Test.cpp
@@ -0,0 +1,33 @@
+/*
+ * EXP2TEST.cpp
+ *
+ *  Created on: Dec 27, 2015
+ *      Author: farbod
+ */
+
+#include "test/EXP2.h"
+
+
+int main (int argc, char* argv[])
+{
+
+	// subsystem dynamics
+	std::vector<std::shared_ptr<ControlledSystemBase<2,1> > > subsystemDynamicsPtr {std::make_shared<EXP2_Sys1>(), std::make_shared<EXP2_Sys2>()};
+
+	// subsystem derivatives
+	std::vector<std::shared_ptr<DerivativesBase<2,1> > > subsystemDerivativesPtr {std::make_shared<EXP2_SysDerivative1>(), std::make_shared<EXP2_SysDerivative2>()};
+
+	// subsystem cost functions
+	std::vector<std::shared_ptr<CostFunctionBase<2,1> > > subsystemCostFunctionsPtr {std::make_shared<EXP2_CostFunction1>(), std::make_shared<EXP2_CostFunction2>()};
+
+	GLQP<2,1>::state_vector_array_t   stateOperatingPoints(2, Eigen::Matrix<double,2,1>::Zero());;
+	GLQP<2,1>::control_vector_array_t inputOperatingPoints(2, Eigen::Matrix<double,1,1>::Zero());;
+	std::vector<size_t> systemStockIndex {0, 1};
+
+	// GLQP
+	GLQP<2,1> glqp(subsystemDynamicsPtr, subsystemDerivativesPtr, subsystemCostFunctionsPtr, stateOperatingPoints, inputOperatingPoints, systemStockIndex);
+
+	std::vector<double> switchingTimes {0, 0.8, 2};
+	glqp.SolveRiccatiEquation(switchingTimes);
+
+}
